e1166e1c0763240bd57e7ffb9e9b665fd669bb7c
Nguyễn Minh Đức <114391544+110100100100@users.noreply.github.com>
2024-04-15 13:11:52 +0700

    Add files via upload

---
+++ AddIndex/Program.cs
@@ -0,0 +1,40 @@
+﻿int[] array = { 1, 2, 3, 4, 5 };
+        
+        
+        Console.WriteLine("Nhập số bạn muốn thêm:");
+        int X = int.Parse(Console.ReadLine());
+        
+        
+        Console.WriteLine("Nhập phần tử bạn muốn chèn:");
+        int index = int.Parse(Console.ReadLine());
+        
+        
+        if (index < 0 || index >= array.Length)
+        {
+            Console.WriteLine("Không thể chèn phần tử vào mảng tại vị trí bạn muốn.");
+            return;
+        }
+        
+       
+        int[] newArray = new int[array.Length + 1];
+        
+      
+        for (int i = 0, j = 0; i < newArray.Length; i++)
+        {
+            if (i == index)
+            {
+                newArray[i] = X;
+            }
+            else
+            {
+                newArray[i] = array[j];
+                j++;
+            }
+        }
+        
+       
+        Console.WriteLine("Mảng sau khi chèn phần tử:");
+        foreach (int num in newArray)
+        {
+            Console.Write(num + " ");
+        }
