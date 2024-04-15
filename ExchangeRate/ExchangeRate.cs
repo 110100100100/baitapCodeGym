6d66135998d7e57cda58fdc33f026089c4b14a3e
Nguyễn Minh Đức <114391544+110100100100@users.noreply.github.com>
2024-04-15 08:34:32 +0700

    Add files via upload

---
+++ ExchangeRate/ExchangeRate.cs
@@ -0,0 +1,6 @@
+﻿System.Console.Write("Nhập số tiền USD: ");
+double USD = Convert.ToDouble(Console.ReadLine());
+double exchangeRate = 23000;
+double VND = USD * exchangeRate;
+
+System.Console.WriteLine(USD + "USD tương đương với " + VND + " VND.");
